f8687ab4fb77c5ae0828145d09566c714c5beac9
1605773076 <81518163+1605773076@users.noreply.github.com>
2021-03-28 06:44:28 -0800

    Add files via upload

---
+++ ConsoleApplication2/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Class1
+    {
+        public void equals(int a, int b) {
+            if (a == b)
+                Console.WriteLine("相等");
+            else
+                Console.WriteLine("不相等");
+        }
+        public int jiafa( int x,int y){
+            return x + y;
+        }
+        public int jianfa(int x, int y) {
+            return x - y;
+        }
+        public void chufa(int x, int y) {
+            try
+            {
+                Console.WriteLine (x / y);
+            }
+            catch {
+                Console.WriteLine("输入错误");
+            }
+        }
+        public int chenfa(int x, int y) {
+            return x * y;
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            char c;
+            Class1 d=new Class1();
+           a = Convert.ToInt32(Console.ReadLine());
+           c = Convert.ToChar(Console.ReadLine());
+           b = Convert.ToInt32(Console.ReadLine());
+            if (c == '+') {
+                d.equals(a, b);
+                Console.WriteLine(d.jiafa(a, b));
+
+            }
+
+            if (c == '-') { 
+                d.equals(a, b); 
+                Console.WriteLine( d.jianfa(a, b));
+            }
+
+            if (c == '*') { 
+                Console.WriteLine(d.chenfa(a, b));
+                d.equals(a, b);
+            
+            }
+
+            if (c == '/') {
+                d.chufa(a, b);
+                d.equals(a, b);
+            
+            }
+            Console.ReadKey();
+        }
+        
+    }
+
+}
